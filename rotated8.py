e418e6913f6852c48b2a4e47c599f70e426041f7
Collin Brittle <rotated8@vt.edu>
2014-03-12 22:06:28 -0400

    Update Flask
    Commenting out gunicorn fixes.

--- rotated8.py
+++ rotated8.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from werkzeug.contrib.fixers import ProxyFix
+#from werkzeug.contrib.fixers import ProxyFix
 app = Flask('site')
 
 @app.route('/')
 def hello():
     return 'Hello world!'
 
-app.wsgi_app = ProxyFix(app.wsgi_app)
+#app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     app.run()
